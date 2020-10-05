974cb5afb113bdecbabbf40315c229c47bd143c6
Max <igrekzero@gmail.com>
2020-10-05 23:32:56 +0300

    Laba3

---
+++ OAP/Laba3/KrisanovLaba3/KrisanovLaba3/main.cpp
@@ -0,0 +1,49 @@
+#include <iomanip> 
+#include <iostream>
+
+using namespace std;
+void main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	/*char c, probel;
+	probel = ' ';
+	cout << "¬ведите символ дл€ заполнени€: "; cin >> c;
+	cout << setw(35) << setfill(probel) << probel;
+	cout << setw(3) << setfill(c) << c << endl;
+	cout << setw(35) << setfill(probel) << probel;
+	cout << setw(3) << setfill(c) << c << endl;
+	cout << setw(35) << setfill(probel) << probel;
+	cout << setw(3) << setfill(c) << c << endl;
+	cout << setw(36) << setfill(probel) << probel;
+	cout << setw(1) << setfill(c) << c << endl;
+	cout << setw(34) << setfill(probel) << probel;
+	cout << setw(5) << setfill(c) << c << endl;
+	cout << setw(30) << setfill(probel) << probel;
+	cout << setw(13) << setfill(c) << c << endl;
+	cout << setw(34) << setfill(probel) << probel;
+	cout << setw(5) << setfill(c) << c << endl;
+	cout << setw(34) << setfill(probel) << probel;
+	cout << setw(5) << setfill(c) << c << endl;
+	cout << setw(34) << setfill(probel) << probel;
+	cout << setw(1) << setfill(c) << c;
+	cout << setw(3) << setfill(probel) << probel;
+	cout << setw(1) << setfill(c) << c << endl;
+	cout << setw(34) << setfill(probel) << probel;
+	cout << setw(1) << setfill(c) << c;
+	cout << setw(3) << setfill(probel) << probel;
+	cout << setw(1) << setfill(c) << c << endl;
+	cout << setw(34) << setfill(probel) << probel;
+	cout << setw(1) << setfill(c) << c;
+	cout << setw(3) << setfill(probel) << probel;
+	cout << setw(1) << setfill(c) << c << endl;*/
+
+
+
+	/*int a, b, c;
+	double m, p;
+	cout << "¬ведите стороны треугольника"; cin >> a >> b >> c;
+	p = (a + b + c) / 2;
+	m = sqrt(p * (p - a) * (p - b) * (p - c));
+	cout << m << endl;*/
+}
+
