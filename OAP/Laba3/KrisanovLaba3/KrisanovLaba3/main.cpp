5ade5a2c97319c11c248247a4c543e9981ea22e2
Max <igrekzero@gmail.com>
2020-10-12 12:48:18 +0300

    Laba3

--- OAP/Laba3/KrisanovLaba3/KrisanovLaba3/main.cpp
+++ OAP/Laba3/KrisanovLaba3/KrisanovLaba3/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void main()
 {
 	setlocale(LC_CTYPE, "Russian");
+	// 8 вариант
 	/*char c, probel;
 	probel = ' ';
 	cout << "Введите символ для заполнения: "; cin >> c;
@@ -45,5 +46,95 @@ void main()
 	p = (a + b + c) / 2;
 	m = sqrt(p * (p - a) * (p - b) * (p - c));
 	cout << m << endl;*/
+
+
+	// 1 вариант
+	/*char c, probel;
+	probel = ' ';
+	cout << "Введите символ для заполнения: "; cin >> c;
+	cout << setw(35) << setfill(probel) << probel;
+	cout << setw(2) << setfill(c) << c << endl;
+	cout << setw(34) << setfill(probel) << probel;
+	cout << setw(3) << setfill(c) << c << endl;
+	cout << setw(33) << setfill(probel) << probel;
+	cout << setw(4) << setfill(c) << c << endl;
+	cout << setw(33) << setfill(probel) << probel;
+	cout << setw(4) << setfill(c) << c << endl;
+	cout << setw(34) << setfill(probel) << probel;
+	cout << setw(3) << setfill(c) << c << endl;
+	cout << setw(35) << setfill(probel) << probel;
+	cout << setw(2) << setfill(c) << c << endl;*/
+
+	
+	//int a, b, c;
+	//a = 3;
+	//b = 5;
+	//// 1 способ 
+	////c = a;
+	////a = b;
+	////b = c;
+	////cout << a << "   " << b;
+	//// 2 способ 
+	//swap(a, b);
+	//cout << a << "   " << b;
+
+
+	// 4 вариант
+	/*char c, probel;
+	probel = ' ';
+	cout << "введите символ для заполнения: "; cin >> c;
+	cout << setw(55) << setfill(probel) << probel;
+	cout << setw(4) << setfill(c) << c << endl;
+	cout << setw(53) << setfill(probel) << probel;
+	cout << setw(8) << setfill(c) << c << endl;
+	cout << setw(51) << setfill(probel) << probel;
+	cout << setw(12) << setfill(c) << c << endl;
+	cout << setw(50) << setfill(probel) << probel;
+	cout << setw(14) << setfill(c) << c << endl;
+	cout << setw(50) << setfill(probel) << probel;
+	cout << setw(14) << setfill(c) << c << endl;
+	cout << setw(50) << setfill(probel) << probel;
+	cout << setw(14) << setfill(c) << c << endl;
+	cout << setw(51) << setfill(probel) << probel;
+	cout << setw(12) << setfill(c) << c << endl;
+	cout << setw(53) << setfill(probel) << probel;
+	cout << setw(8) << setfill(c) << c << endl;
+	cout << setw(55) << setfill(probel) << probel;
+	cout << setw(4) << setfill(c) << c << endl;*/
+
+	/*int x, y, z, a;
+	double sredneeAr, sredneeGeo;
+	cout << "Введите x, y, z"; cin >> x >> y >> z;
+	sredneeAr = (x + y + z) / 3.;
+	a = x + y + z;
+	sredneeGeo = pow(a, 1.0 / 3);
+	cout << "Среднее арифмитическое: " << sredneeAr << "   " << "Среднее геометрическое: " << sredneeGeo;*/
+
+	// 11 вариант
+
+//char c, probel;
+//probel = ' ';
+//cout << "введите символ для заполнения: "; cin >> c;
+//cout << setw(55) << setfill(probel) << probel;
+//cout << setw(10) << setfill(c) << c << endl;
+//cout << setw(55) << setfill(probel) << probel;
+//cout << setw(10) << setfill(c) << c << endl;
+//cout << setw(55) << setfill(probel) << probel;
+//cout << setw(10) << setfill(c) << c << endl;
+//cout << setw(55) << setfill(probel) << probel;
+//cout << setw(10) << setfill(c) << c << endl;
+//cout << setw(55) << setfill(probel) << probel;
+//cout << setw(10) << setfill(c) << c << endl;
+//cout << setw(55) << setfill(probel) << probel;
+//cout << setw(10) << setfill(c) << c << endl;
+//cout << setw(55) << setfill(probel) << probel;
+//cout << setw(10) << setfill(c) << c << endl;
+
+	/*int diagonal;
+	double sqr, storona;
+	cout << "Введите диагональ"; cin >> diagonal;
+	storona = sqrt(pow(diagonal, 2) / 2);
+	sqr = pow(storona, 2);
+	cout << sqr;*/
 }
 
